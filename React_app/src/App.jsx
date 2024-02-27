17fc1fcc0fd4e2b29c711c1a3d3375659d52a97d
Rubel <rubel.mozumder@outlook.com>
2024-02-27 16:23:11 +0100

    component and color are working.

--- React_app/src/App.jsx
+++ React_app/src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { CORE_CONCEPTS } from "./data.js";
+import reactCoreConcept from "./assets/react-core-concepts.png";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const reactDescriptors = ['Fundamental', 'Component-based', 'Declarative', 'Learn Once, Write Anywhere']
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
+function Header() {
+  const rendDesc = reactDescriptors[genRandomInt(reactDescriptors.length)]
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React ! HAHAHA</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+    <header>
+      <img src={reactCoreConcept} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {rendDesc} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function ReactConcept(props) {
+  return (
+    <li>
+      <h2>{props.title}</h2>
+      <img src={props.img} alt="React logo" />
+      <p>{props.concept}</p>
+    </li>
+  );
+}
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+  return (
+    <div id="root">
+      <Header />
+      <main>  
+        <section id="core_concepts">
+          <ul>
+            <ReactConcept title={CORE_CONCEPTS[0].title} 
+                          img={CORE_CONCEPTS[0].image} 
+                          concept={CORE_CONCEPTS[0].description} />
+            <ReactConcept title={CORE_CONCEPTS[1].title} 
+                          img={CORE_CONCEPTS[1].image} 
+                          concept={CORE_CONCEPTS[1].description} />
+            <ReactConcept title={CORE_CONCEPTS[2].title} 
+                          img={CORE_CONCEPTS[2].image} 
+                          concept={CORE_CONCEPTS[2].description} />
+            <ReactConcept title={CORE_CONCEPTS[3].title} 
+                          img={CORE_CONCEPTS[3].image} 
+                          concept={CORE_CONCEPTS[3].description} />
+          </ul>
+        </section>
+
+        {/* <reactConcept title="Learn Once, Write Anywhere" img={reactLogo} concept="Learn Once, Write Anywhere" />
+        <reactConcept title="Fundamental" img={reactLogo} concept="Fundamental" /> */}
+      </main>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
+export { Header, ReactConcept };
